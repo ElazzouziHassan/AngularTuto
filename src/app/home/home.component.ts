3a929ddbfbcff45fdac9cda712b900cdb7f2c5f2
wizardy <wizardhassan99@gmail.com>
2023-12-20 20:05:48 +0100

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,20 +12,9 @@ import { NavbarComponent } from '../components/navbar/navbar.components';
     HousingLocationComponent,
     NavbarComponent
   ],
-  template: `<app-navbar></app-navbar>`,
+  templateUrl: './home.componet.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-
-  housinglocation: Housinglocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+ 
 }
