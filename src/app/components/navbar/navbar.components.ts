8b601589b410aa1d46323cb1631383b960e3b827
wizardy <wizardhassan99@gmail.com>
2023-12-20 19:49:53 +0100

    commit

---
+++ src/app/components/navbar/navbar.components.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  template: `
+    
+  `,
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {
+
+  
+}
