b4ac3c62ed58ef6712cd28cb21f65a248373982f
wizardy <wizardhassan99@gmail.com>
2023-12-22 20:41:12 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,13 @@ import { AboutComponent } from './pages/about/about.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { OthersComponent } from './pages/others/others.component';
 import { LoginComponent } from './login/login.component';
-// import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  // { path: 'docs', component: DocsComponent, canActivate: [AuthGuard] },
-  // { path: 'others', component: OthersComponent, canActivate: [AuthGuard] },
+  { path: 'docs', component: DocsComponent, canActivate: [AuthGuard] },
+  { path: 'others', component: OthersComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' } 
