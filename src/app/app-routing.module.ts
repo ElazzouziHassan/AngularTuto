f1461f37ebe55005cd6a0adda55f60dec9f35505
wizardy <wizardhassan99@gmail.com>
2023-12-22 20:33:45 +0100

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DocsComponent } from './pages/docs/docs.component';
+import { OthersComponent } from './pages/others/others.component';
+import { LoginComponent } from './login/login.component';
+// import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  // { path: 'docs', component: DocsComponent, canActivate: [AuthGuard] },
+  // { path: 'others', component: OthersComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
