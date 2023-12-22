15c19b583144f018282a81dd8f8e997d1656847e
wizardy <wizardhassan99@gmail.com>
2023-12-22 20:42:23 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { OthersComponent } from './pages/others/others.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
