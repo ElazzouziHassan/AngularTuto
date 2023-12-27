7cbb72d5b51d612193b3aabe24efabdf1eb5ada9
wizardy <wizardhassan99@gmail.com>
2023-12-27 20:55:58 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DocsComponent } from './pages/docs/docs.component';
+import { OthersComponent } from './pages/others/others.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'/docs', component: DocsComponent},
+  {path:'/others', component: OthersComponent},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  exports: [RouterModule]
+})
+
+export class appRoutingModule {}
