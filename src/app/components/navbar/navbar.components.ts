f8bdcf4d9e684e66fa3f9ff507b9fede422edccb
wizardy <wizardhassan99@gmail.com>
2023-12-29 18:29:53 +0100

    commit

--- src/app/components/navbar/navbar.components.ts
+++ src/app/components/navbar/navbar.components.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../../pages/home/home.component';
+import { AboutComponent } from '../../pages/about/about.component';
+import { DocsComponent } from '../../pages/docs/docs.component';
+import { OthersComponent } from '../../pages/others/others.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule, 
+    HomeComponent,
+    AboutComponent,
+    DocsComponent,
+    OthersComponent,
+    RouterLink
   ],
   templateUrl: './navbar.components.html',
   styleUrls: ['./navbar.component.css'],
